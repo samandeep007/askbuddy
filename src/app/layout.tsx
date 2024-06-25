feb4498fc73dc191cd13b5ed54e4be62ec9c790e
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-06-24 22:42:06 -0600

    chore: Update .env file with NEXTAUTH_SECRET value
    ```

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import AuthProvider from '@/context/AuthProvider'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -16,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <AuthProvider>
       <body className={inter.className}>{children}</body>
+      </AuthProvider>
     </html>
   )
 }
