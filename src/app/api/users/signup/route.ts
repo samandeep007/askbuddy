ca9f15010c3ac20747dd10d6dd69279dd537774d
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-06-24 19:32:10 -0600

    chore: Refactor signup route to improve code readability and maintainability

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,70 +1,83 @@
 import dbConnect from "@/lib/dbConnect";
-import {NextRequest, NextResponse} from 'next/server';
-import UserModel  from "@/model/User";
-import bcrypt from 'bcrypt';
+import UserModel from "@/model/User";
+import bcrypt from "bcrypt";
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 
-dbConnect();
+export const POST = async (request: Request) => {
+    await dbConnect();
 
-export const POST = async(request: NextRequest) => {
     try {
         const reqBody = await request.json();
-        const{username, email, password} = reqBody;
+        const { username, email, password } = reqBody;
 
-        const userExists = await UserModel.findOne({email: email, username: username})
+        const userExists = await UserModel.findOne({
+            email: email,
+            username: username,
+        });
 
-        if(userExists){
-            if(userExists.isVerified){
-                return NextResponse.json({
-                    status: 400,
-                    success: false,
-                    message: "User with these credentials already exist"
-                })
+        if (userExists) {
+            if (userExists.isVerified) {
+                return Response.json(
+                    {
+                        success: false,
+                        message: "User with these credentials already exist",
+                    },
+                    {
+                        status: 400,
+                    }
+                );
+            } else {
+                await bcrypt
+                    .hash(password, 10)
+                    .then((result) => (userExists.password = result));
+                userExists.verifyCode = String(Math.floor(Math.random() * 1000000 + 1));
+                const savedUser = await userExists.save({ validateBeforeSave: false });
+
+                return Response.json(
+                    {
+                        success: true,
+                        data: savedUser,
+                        message: "User updated successfully",
+                    },
+                    {
+                        status: 200,
+                    }
+                );
             }
-            else {
-                await bcrypt.hash(password, 10).then(result => userExists.password = result);
-                userExists.verifyCode = String(Math.floor(Math.random()*1000000 + 1))
-                const savedUser = await userExists.save({validateBeforeSave: false});
-
-                return NextResponse.json({
-                    success: true,
-                    status: 200,
-                    data: savedUser,
-                    message: "User updated successfully"
-                })
-            }
-        }
-        else {
-
+        } else {
             const salt = await bcrypt.genSalt(10);
-        const encryptedPassword = await bcrypt.hash(password, salt);
-            
+            const encryptedPassword = await bcrypt.hash(password, salt);
 
             const newUser = new UserModel({
                 username: username,
                 email: email,
                 password: encryptedPassword,
-                verifyCode:Math.floor(Math.random()*1000000),
-                verifyCodeExpiry: Date.now() + 3600000
-            })
+                verifyCode: Math.floor(Math.random() * 1000000),
+                verifyCodeExpiry: Date.now() + 3600000,
+            });
 
-            const savedUser = await newUser.save()
-            return NextResponse.json({
-                success: true,
-                status: 200,
-                data: savedUser,
-                message: "User signed-up successfully",
-             
-
-            })
+            const savedUser = await newUser.save();
+            return Response.json(
+                {
+                    success: true,
+                    data: savedUser,
+                    message: "User signed-up successfully",
+                },
+                {
+                    status: 200,
+                }
+            );
         }
-
-        
-    } catch (error:any) {
-        throw NextResponse.json({
-            status: 400,
-            success: false,
-            message: error.message
-        })
+    } catch (error: any) {
+        console.log();
+        return Response.json(
+            {
+                success: false,
+                message: error.message,
+            },
+            {
+                status: 400,
+            }
+        );
     }
-}
-
+};
