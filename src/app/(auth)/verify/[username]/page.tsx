cd3f57971b014f438b3bed482167caadfac7bc1a
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-06-25 23:41:54 -0600

    chore: Update verify page to handle form submission and display error messages

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -5,24 +5,42 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod'
-import axios from 'axios'
+import axios, { AxiosError }  from 'axios'
+import { ApiResponse } from '@/types/ApiResponse'
 
 export default function page() {
   const router = useRouter()
-  const params = useParams<{username: string}>()
-  const {toast} = useToast()
+  const params = useParams<{ username: string }>()
+  const { toast } = useToast()
 
   const form = useForm<z.infer<typeof verifySchema>>({
     resolver: zodResolver(verifySchema)
-})
+  })
 
-const onSubmit = async(data: z.infer<typeof verifySchema>) => {
-  try {
-    await axios.post('/api/verify-code', {username: params.username, code: data.code})
-  } catch (error) {
-    
+  const onSubmit = async (data: z.infer<typeof verifySchema>) => {
+    try {
+      const response = await axios.post('/api/verify-code', { 
+        username: params.username, 
+        code: data.code })
+
+      toast({
+        title: "Success",
+        description: response.data.message
+      })
+
+      router.replace('sign-in')
+    } catch (error) {
+      console.error("Error in signup of user", error)
+      const axiosError = error as AxiosError<ApiResponse>
+      const errorMessage = axiosError.response?.data.message;
+      toast({
+          title: "Signup failed",
+          description: errorMessage,
+          variant: "destructive"
+      })
+
+    }
   }
-}
   return (
     <div>page</div>
   )
