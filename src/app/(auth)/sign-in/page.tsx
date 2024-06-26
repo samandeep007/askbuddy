fd6d7696fbb4d85ea31cfbec670725e794df4c7c
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-06-26 09:45:17 -0600

    Refactor sign-in page component to use password input type

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -97,7 +97,7 @@ export default function SigninPage() {
                 <FormItem>
                   <FormLabel>Password</FormLabel>
                   <FormControl>
-                    <Input placeholder="password" {...field}
+                    <Input type="password" placeholder="password" {...field}
                       onChange={e => {
                         field.onChange(e);
                       }} />
