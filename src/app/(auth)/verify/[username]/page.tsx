debbd4fe1b30b996ee598893e47280a0481dd74b
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-06-25 23:46:49 -0600

    chore: Update verify page to include form for account verification

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -7,6 +7,10 @@ import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod'
 import axios, { AxiosError }  from 'axios'
 import { ApiResponse } from '@/types/ApiResponse'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Button } from '@react-email/components'
+import { Input } from '@/components/ui/input'
+
 
 export default function page() {
   const router = useRouter()
@@ -42,6 +46,37 @@ export default function page() {
     }
   }
   return (
-    <div>page</div>
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+    <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+    <div className="text-center">
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Verify your account
+        </h1>
+        <p className="mb-4">You're just a step away from us</p>
+        <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                    <FormField
+                        name="code"
+                        control={form.control}
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>OTP</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="One-time Password" {...field}
+                                        onChange={e => {
+                                            field.onChange(e);
+                                        }} />
+                                </FormControl>
+                            </FormItem>
+                        )}
+                    />
+                 
+                    <Button type="submit">Submit</Button>
+                </form>
+            </Form>
+    </div>
+    </div>
+    </div>
+   
   )
 }
