08227f6ed524c417e129409c07f20c41e2515531
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-06-25 23:55:37 -0600

    chore: Refactor verify page component and handle form submission

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod'
-import axios, { AxiosError }  from 'axios'
+import axios, { AxiosError } from 'axios'
 import { ApiResponse } from '@/types/ApiResponse'
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Button } from '@react-email/components'
@@ -25,9 +25,10 @@ export default function VerifyPage() {
 
   const onSubmit = async (data: z.infer<typeof verifySchema>) => {
     try {
-      const response = await axios.post('/api/verify-code', { 
-        username: params.username, 
-        code: data.code })
+      const response = await axios.post('/api/verify-code', {
+        username: params.username,
+        code: data.code
+      })
 
       toast({
         title: "Success",
@@ -40,45 +41,45 @@ export default function VerifyPage() {
       const axiosError = error as AxiosError<ApiResponse>
       const errorMessage = axiosError.response?.data.message;
       toast({
-          title: "Signup failed",
-          description: errorMessage,
-          variant: "destructive"
+        title: "Signup failed",
+        description: errorMessage,
+        variant: "destructive"
       })
 
     }
   }
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-800">
-    <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
-    <div className="text-center">
-        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
             Verify your account
-        </h1>
-        <p className="mb-4">You're just a step away from us</p>
-        <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-                    <FormField
-                        name="code"
-                        control={form.control}
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>OTP</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="One-time Password" {...field}
-                                        onChange={e => {
-                                            field.onChange(e);
-                                        }} />
-                                </FormControl>
-                            </FormItem>
-                        )}
-                    />
-                 
-                    <Button type="submit">Submit</Button>
-                </form>
-            </Form>
+          </h1>
+          <p className="mb-4">You're just a step away from us</p>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <FormField
+                name="code"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>OTP</FormLabel>
+                    <FormControl>
+                      <Input placeholder="One-time Password" {...field}
+                        onChange={e => {
+                          field.onChange(e);
+                        }} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-   
+
   )
 }
