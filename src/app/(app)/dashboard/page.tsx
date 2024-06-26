5d0767abb83a89a51abc397af04bfb0242a39d66
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-06-26 14:19:23 -0600

    chore: Update npm dependencies and add Embla Carousel React component

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -11,6 +11,10 @@ import axios, { AxiosResponse } from 'axios'
 import { AxiosError } from "axios";
 import { ApiResponse } from "@/types/ApiResponse";
 import { User } from "next-auth";
+import { Button } from "@/components/ui/button";
+import { Switch } from "@/components/ui/switch";
+import { Separator } from "@/components/ui/separator";
+import { Loader2, RefreshCcw } from "lucide-react";
 
 
 export default function DashboardPage() {
@@ -116,7 +120,7 @@ export default function DashboardPage() {
   const copyToClipboard = () => {
     navigator.clipboard.writeText(profileUrl)
     toast({
-      title: "Url copied to clipboard",
+      title: "Url Copied!",
       description: "Profile URL has been copied to clipboard"
     })
   }
@@ -131,6 +135,62 @@ export default function DashboardPage() {
   
 
   return (
-    <div>page</div>
+    <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
+    <h1 className="text-4xl font-bold mb-4">User Dashboard</h1>
+
+    <div className="mb-4">
+      <h2 className="text-lg font-semibold mb-2">Copy Your Unique Link</h2>{' '}
+      <div className="flex items-center">
+        <input
+          type="text"
+          value={profileUrl}
+          disabled
+          className="input input-bordered w-full p-2 mr-2"
+        />
+        <Button onClick={copyToClipboard}>Copy</Button>
+      </div>
+    </div>
+
+    <div className="mb-4">
+      <Switch
+        {...register('acceptMessages')}
+        checked={acceptMessages}
+        onCheckedChange={handleSwitchChange}
+        disabled={isSwitchLoading}
+      />
+      <span className="ml-2">
+        Accept Messages: {acceptMessages ? 'On' : 'Off'}
+      </span>
+    </div>
+    <Separator />
+
+    <Button
+      className="mt-4"
+      variant="outline"
+      onClick={(e) => {
+        e.preventDefault();
+        fetchMessages(true);
+      }}
+    >
+      {isLoading ? (
+        <Loader2 className="h-4 w-4 animate-spin" />
+      ) : (
+        <RefreshCcw className="h-4 w-4" />
+      )}
+    </Button>
+    <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
+      {messages.length > 0 ? (
+        messages.map((message, index) => (
+          <MessageCard
+            key={message._id}
+            message={message}
+            onMessageDelete={handleDeleteMessage}
+          />
+        ))
+      ) : (
+        <p>No messages to display.</p>
+      )}
+    </div>
+  </div>
   )
 }
